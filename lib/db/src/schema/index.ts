18ef51e7b219bd67b0fff1ab3a1699a3320016f2
Bilal Zaidi <team.futuramalabs@gmail.com>
2026-06-02 23:34:41 +0500

    feat(db): export platform-cost-models and billing-records schemas

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -3,3 +3,5 @@ export * from "./platforms";
 export * from "./campaigns";
 export * from "./transactions";
 export * from "./auth";
+export * from "./platform-cost-models";
+export * from "./billing-records";
